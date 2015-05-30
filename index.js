0a86444a2cbb6ead21f2d1bd1c28b80237733ada
Roy Evan Sia <roy.evan.sia@gmail.com>
2015-05-30 09:04:25 +0800

    Fix audio not played successively

--- index.js
+++ index.js
@@ -6,22 +6,19 @@ import path from 'path';
 
 const KEYS = {
   DELETE: 8,
-  ENTER: 13,
-  SPACE: 32
+  ENTER : 13,
+  SPACE : 32
 };
 
-let getAudio = memoize(function(keyCode) {
-  let name;
+const AUDIO_MAP = {
+  [KEYS.DELETE]: 'delete_press.mp3',
+  [KEYS.ENTER] : 'spacebar_press.mp3',
+  [KEYS.SPACE] : 'spacebar_press.mp3',
+  DEFAULT      : 'key_press.mp3'
+};
 
-  switch (keyCode) {
-    case KEYS.ENTER :
-    case KEYS.SPACE : name = 'spacebar_press.mp3'; break;
-    case KEYS.DELETE: name = 'delete_press.mp3'; break;
-    default         : name = 'key_press.mp3'; break;
-  }
-
-  return new Audio(path.join(__dirname, 'audio', name));
-}, { primitive: true });
+let getAudio = memoize(name => new Audio(path.join(__dirname, 'audio', name)),
+                       { primitive: true });
 
 export default {
   subscriptions: null,
@@ -44,8 +41,9 @@ export default {
     this.subscriptions.dispose();
   },
 
-  handleKeyDown(e) {
-    let audio = getAudio(e.keyCode);
+  handleKeyDown({ keyCode }) {
+    let audio = getAudio(AUDIO_MAP[keyCode] || AUDIO_MAP.DEFAULT);
+    audio.currentTime = 0;
     audio.play();
   }
 };
